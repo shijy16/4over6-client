9fddab72edb9330c578d15ba5b2dec0b99a7dfe0
shijy16 <745921992@qq.com>
2019-05-03 18:09:14 +0800

    linux可以运行，不确定正确性，手机上暂时跑不了

--- Over6Client/app/src/main/jni/util.h
+++ Over6Client/app/src/main/jni/util.h
@@ -8,12 +8,18 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include<arpa/inet.h>
+#include <pthread.h>
 
 #include "over6_over6client_MainActivity.h"
 
 #define SERVER_ADDR "2402:f000:4:72:808::6b04"
 #define SERVER_PORT 5678
 #define IP_REQ 100
+#define IP_REP 101
+#define DATA_SEND 102
+#define DATA_RECV 103
+#define HEARTBEAT 104
+
 #define LOG(...) __android_log_print(ANDROID_LOG_DEBUG,"jni_C",__VA_ARGS__)
 
 struct Msg {
